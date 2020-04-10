7bc3199dcf0422e72b808960050e833d40e3237f
Dennis <dennis12hahn@gmail.com>
2020-04-10 12:12:16 -0400

    update create logo and edit logo screen

--- client/src/components/CreateLogoScreen.js
+++ client/src/components/CreateLogoScreen.js
@@ -1,17 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import gql from "graphql-tag";
-import { Mutation } from "react-apollo";
-import { Link } from 'react-router-dom';
+import {Mutation} from "react-apollo";
+import {Link} from 'react-router-dom';
 
 const ADD_LOGO = gql`
     mutation AddLogo(
         $text: String!,
         $color: String!,
-        $fontSize: Int!) {
+        $fontSize: Int!,
+        $backgroundColor: String!,
+        $borderColor: String!,
+        $borderRadius: Int!,
+        $borderWidth: Int!,
+        $padding: Int!,
+        $margin: Int!) {
         addLogo(
             text: $text,
             color: $color,
-            fontSize: $fontSize) {
+            fontSize: $fontSize
+            fontSize: $fontSize
+            backgroundColor: $backgroundColor,
+            borderColor: $borderColor,
+            borderRadius: $borderRadius,
+            borderWidth: $borderWidth,
+            padding: $padding,
+            margin: $margin) {
             _id
         }
     }
@@ -20,43 +33,100 @@ const ADD_LOGO = gql`
 class CreateLogoScreen extends Component {
 
     render() {
-        let text, color, fontSize;
+        let text, color, fontSize, backgroundColor,
+            borderColor, borderRadius, borderWidth,
+            padding, margin;
+
         return (
             <Mutation mutation={ADD_LOGO} onCompleted={() => this.props.history.push('/')}>
-                {(addLogo, { loading, error }) => (
+                {(addLogo, {loading, error}) => (
                     <div className="container">
                         <div className="panel panel-default">
                             <div className="panel-heading">
                                 <h4><Link to="/">Home</Link></h4>
                                 <h3 className="panel-title">
                                     Create Logo
-                            </h3>
+                                </h3>
                             </div>
                             <div className="panel-body">
                                 <form onSubmit={e => {
                                     e.preventDefault();
-                                    addLogo({ variables: { text: text.value, color: color.value, fontSize: parseInt(fontSize.value) } });
+                                    addLogo({
+                                        variables: {
+                                            text: text.value,
+                                            color: color.value,
+                                            fontSize: parseInt(fontSize.value),
+                                            backgroundColor: backgroundColor.value,
+                                            borderColor: borderColor.value,
+                                            borderRadius: borderRadius.value,
+                                            borderWidth: borderWidth.value,
+                                            padding: padding.value,
+                                            margin: margin.value
+                                        }
+                                    });
                                     text.value = "";
                                     color.value = "";
                                     fontSize.value = "";
+                                    backgroundColor.value = "";
+                                    borderColor.value = "";
+                                    borderRadius.value = "";
+                                    borderWidth.value = "";
+                                    padding.value = "";
+                                    margin.value = "";
                                 }}>
                                     <div className="form-group">
                                         <label htmlFor="text">Text:</label>
                                         <input type="text" className="form-control" name="text" ref={node => {
                                             text = node;
-                                        }} placeholder="Text" />
+                                        }} placeholder="Text"/>
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="color">Color:</label>
                                         <input type="color" className="form-control" name="color" ref={node => {
                                             color = node;
-                                        }} placeholder="Color" />
+                                        }} placeholder="Color"/>
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="fontSize">Font Size:</label>
                                         <input type="number" className="form-control" name="fontSize" ref={node => {
                                             fontSize = node;
-                                        }} placeholder="Font Size" />
+                                        }} placeholder="Font Size"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="backgroundColor">Background Color:</label>
+                                        <input type="color" className="form-control" name="backgroundColor" ref={node => {
+                                            backgroundColor = node;
+                                        }} placeholder="Background Color"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="borderColor">Border Color:</label>
+                                        <input type="color" className="form-control" name="borderColor" ref={node => {
+                                            borderColor = node;
+                                        }} placeholder="Border Color"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="borderRadius">Border Radius:</label>
+                                        <input type="number" className="form-control" name="borderRadius" ref={node => {
+                                            borderRadius = node;
+                                        }} placeholder="Border Radius"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="borderWidth">Border Width:</label>
+                                        <input type="number" className="form-control" name="borderWidth" ref={node => {
+                                            borderWidth = node;
+                                        }} placeholder="Border Width"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="padding">Padding:</label>
+                                        <input type="number" className="form-control" name="padding" ref={node => {
+                                            padding = node;
+                                        }} placeholder="Padding"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="margin">Margin:</label>
+                                        <input type="number" className="form-control" name="margin" ref={node => {
+                                            margin = node;
+                                        }} placeholder="Margin"/>
                                     </div>
                                     <button type="submit" className="btn btn-success">Submit</button>
                                 </form>
--- client/src/components/EditLogoScreen.js
+++ client/src/components/EditLogoScreen.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import gql from "graphql-tag";
-import { Query, Mutation } from "react-apollo";
+import {Query, Mutation} from "react-apollo";
 
 const GET_LOGO = gql`
     query logo($logoId: String) {
@@ -10,6 +10,13 @@ const GET_LOGO = gql`
             text
             color
             fontSize
+            backgroundColor
+            borderColor
+            borderRadius
+            borderWidth
+            padding
+            margin
+            lastUpdate
         }
     }
 `;
@@ -19,12 +26,24 @@ const UPDATE_LOGO = gql`
         $id: String!,
         $text: String!,
         $color: String!,
-        $fontSize: Int!) {
+        $fontSize: Int!,
+        $backgroundColor: String!,
+        $borderColor: String!,
+        $borderRadius: Int!,
+        $borderWidth: Int!,
+        $padding: Int!,
+        $margin: Int!) {
             updateLogo(
                 id: $id,
                 text: $text,
                 color: $color,
-                fontSize: $fontSize) {
+                fontSize: $fontSize
+                backgroundColor: $backgroundColor,
+                borderColor: $borderColor,
+                borderRadius: $borderRadius,
+                borderWidth: $borderWidth,
+                padding: $padding,
+                margin: $margin) {
                     lastUpdate
                 }
         }
@@ -33,49 +52,113 @@ const UPDATE_LOGO = gql`
 class EditLogoScreen extends Component {
 
     render() {
-        let text, color, fontSize;
+        let text, color, fontSize, backgroundColor,
+            borderColor, borderRadius, borderWidth,
+            padding, margin;
+
         return (
-            <Query query={GET_LOGO} variables={{ logoId: this.props.match.params.id }}>
-                {({ loading, error, data }) => {
+            <Query query={GET_LOGO} variables={{logoId: this.props.match.params.id}}>
+                {({loading, error, data}) => {
                     if (loading) return 'Loading...';
                     if (error) return `Error! ${error.message}`;
 
                     return (
-                        <Mutation mutation={UPDATE_LOGO} key={data.logo._id} onCompleted={() => this.props.history.push(`/`)}>
-                            {(updateLogo, { loading, error }) => (
+                        <Mutation mutation={UPDATE_LOGO} key={data.logo._id}
+                                  onCompleted={() => this.props.history.push(`/`)}>
+                            {(updateLogo, {loading, error}) => (
                                 <div className="container">
                                     <div className="panel panel-default">
                                         <div className="panel-heading">
                                             <h4><Link to="/">Home</Link></h4>
                                             <h3 className="panel-title">
                                                 Edit Logo
-                                        </h3>
+                                            </h3>
                                         </div>
-                                        <div className="panel-body">                                            
+                                        <div className="panel-body">
                                             <form onSubmit={e => {
                                                 e.preventDefault();
-                                                updateLogo({ variables: { id: data.logo._id, text: text.value, color: color.value, fontSize: parseInt(fontSize.value) } });
+                                                updateLogo({
+                                                    variables: {
+                                                        id: data.logo._id,
+                                                        text: text.value,
+                                                        color: color.value,
+                                                        fontSize: parseInt(fontSize.value),
+                                                        backgroundColor: backgroundColor.value,
+                                                        borderColor: borderColor.value,
+                                                        borderRadius: borderRadius.value,
+                                                        borderWidth: borderWidth.value,
+                                                        padding: padding.value,
+                                                        margin: margin.value
+                                                    }
+                                                });
                                                 text.value = "";
                                                 color.value = "";
                                                 fontSize.value = "";
+                                                backgroundColor.value = "";
+                                                borderColor.value = "";
+                                                borderRadius.value = "";
+                                                borderWidth.value = "";
+                                                padding.value = "";
+                                                margin.value = "";
                                             }}>
                                                 <div className="form-group">
                                                     <label htmlFor="text">Text:</label>
-                                                    <input type="text" className="form-control" name="text" ref={node => {
-                                                        text = node;
-                                                    }} placeholder="Text" defaultValue={data.logo.text} />
+                                                    <input type="text" className="form-control" name="text"
+                                                           ref={node => {
+                                                               text = node;
+                                                           }} placeholder="Text" defaultValue={data.logo.text}/>
                                                 </div>
                                                 <div className="form-group">
                                                     <label htmlFor="color">Color:</label>
-                                                    <input type="color" className="form-control" name="color" ref={node => {
-                                                        color = node;
-                                                    }} placeholder="Color" defaultValue={data.logo.color} />
+                                                    <input type="color" className="form-control" name="color"
+                                                           ref={node => {
+                                                               color = node;
+                                                           }} placeholder="Color" defaultValue={data.logo.color}/>
                                                 </div>
                                                 <div className="form-group">
                                                     <label htmlFor="fontSize">Font Size:</label>
-                                                    <input type="text" className="form-control" name="fontSize" ref={node => {
-                                                        fontSize = node;
-                                                    }} placeholder="Font Size" defaultValue={data.logo.fontSize} />
+                                                    <input type="text" className="form-control" name="fontSize"
+                                                           ref={node => {
+                                                               fontSize = node;
+                                                           }} placeholder="Font Size"
+                                                           defaultValue={data.logo.fontSize}/>
+                                                </div>
+                                                <div className="form-group">
+                                                    <label htmlFor="backgroundColor">Background Color:</label>
+                                                    <input type="color" className="form-control" name="backgroundColor"
+                                                           ref={node => {
+                                                               backgroundColor = node;
+                                                           }} placeholder="Background Color" defaultValue={data.logo.backgroundColor}/>
+                                                </div>
+                                                <div className="form-group">
+                                                    <label htmlFor="borderColor">Border Color:</label>
+                                                    <input type="color" className="form-control" name="borderColor" ref={node => {
+                                                        borderColor = node;
+                                                    }} placeholder="Border Color"/>
+                                                </div>
+                                                <div className="form-group">
+                                                    <label htmlFor="borderRadius">Border Radius:</label>
+                                                    <input type="number" className="form-control" name="borderRadius" ref={node => {
+                                                        borderRadius = node;
+                                                    }} placeholder="Border Radius"/>
+                                                </div>
+                                                <div className="form-group">
+                                                    <label htmlFor="borderWidth">Border Width:</label>
+                                                    <input type="number" className="form-control" name="borderWidth" ref={node => {
+                                                        borderWidth = node;
+                                                    }} placeholder="Border Width"/>
+                                                </div>
+                                                <div className="form-group">
+                                                    <label htmlFor="padding">Padding:</label>
+                                                    <input type="number" className="form-control" name="padding" ref={node => {
+                                                        padding = node;
+                                                    }} placeholder="Padding"/>
+                                                </div>
+                                                <div className="form-group">
+                                                    <label htmlFor="margin">Margin:</label>
+                                                    <input type="number" className="form-control" name="margin" ref={node => {
+                                                        margin = node;
+                                                    }} placeholder="Margin"/>
                                                 </div>
                                                 <button type="submit" className="btn btn-success">Submit</button>
                                             </form>
