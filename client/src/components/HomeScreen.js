df8bb38223c7da1ab68f9538ea13a407f55e431e
Dennis <dennis12hahn@gmail.com>
2020-04-10 11:54:25 -0400

    fix home screen

--- client/src/components/HomeScreen.js
+++ client/src/components/HomeScreen.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import '../App.css';
 import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
+import {Query} from 'react-apollo';
 
 const GET_LOGOS = gql`
   {
@@ -19,35 +19,39 @@ class HomeScreen extends Component {
     render() {
         return (
             <Query pollInterval={500} query={GET_LOGOS}>
-                {({ loading, error, data }) => {
+                {({loading, error, data}) => {
                     if (loading) return 'Loading...';
                     if (error) return `Error! ${error.message}`;
 
                     return (
                         <div className="container row">
-                            <div className="col s4">
-                                <h3>Recent Work</h3>
-                                {data.logos.map((logo, index) => (
-                                    <div key={index} className='home_logo_link'
-                                        style={{ cursor: "pointer" }}>
-                                        <Link to={`/view/${logo._id}`}>{logo.text}</Link>
-                                    </div>
-                                ))}
+                            <div className="col s4 text-center">
+                                <div className="card" style={{margin: "10%"}}>
+                                    <h3 className="card-header">Recent Work</h3>
+                                    <ul className="list-group list-group-flush">
+                                        {data.logos.map((logo, index) => (
+                                            <div key={index} className='home_logo_link list-group-item'
+                                                 style={{cursor: "pointer"}}>
+                                                <Link style={{fontSize: "large"}} to={`/view/${logo._id}`}>{logo.text}</Link>
+                                            </div>
+                                        ))}
+                                    </ul>
+                                </div>
                             </div>
-                            <div className="col s8">
+                            <div className="col s8 text-center">
                                 <div id="home_banner_container">
-                                    @todo<br />
-                                    List Maker
+                                    goLogoLo
                                 </div>
                                 <div>
-                                    <Link id="add_logo_button" to="/create">Add Logo</Link>
+                                    <Link className="btn btn-primary btn-lg" id="add_logo_button" to="/create">Add
+                                        Logo</Link>
                                 </div>
                             </div>
                         </div>
                     );
                 }
                 }
-            </Query >
+            </Query>
         );
     }
 }
